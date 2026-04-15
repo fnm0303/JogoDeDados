6895f690dd261c1f807d7d80298b370485681c30
Felipe Neto Mello <fnm03br@gmail.com>
2026-04-14 21:12:26 -0300

    Refatorado rodadas dos jogadores

--- Dados.ConsoleApp/Program.cs
+++ Dados.ConsoleApp/Program.cs
@@ -28,79 +28,100 @@ casas.
 ○ Recuo: Se o competidor parar em outra posição específica (ex.: 7, 13, 20), ele recua -2 casas.
 ○ Rodada extra: Se o competidor tirar 6 no dado, ele ganha uma rodada extra.
 */
-//Console.Clear();
-Console.WriteLine("---------- JOGO DE DADOS ------------");
-
-int posicaoJogador1 = 0, posicaoJogador2 = 0, dadoJogador1, dadoJogador2, numeroRodada = 0;
-Random dado = new Random();
-
-do
+class Program
 {
-    Console.WriteLine("\nPressione ENTER para começar a próxima rodada!");
-    Console.ReadLine();
-    numeroRodada += 1;
-    Console.WriteLine($"Rodada nº: {numeroRodada}");
-
-    dadoJogador1 = dado.Next(1, 7);
-    posicaoJogador1 += dadoJogador1;
-
-    if (dadoJogador1 == 6)
+    static void Main(string[] args)
     {
-        Console.WriteLine($"Legal, você tirou {dadoJogador1} e ganhou uma rodada extra.");
-        dadoJogador1 = dado.Next(1, 7);
+        Console.Clear();
+        Console.WriteLine("---------- JOGO DE DADOS ------------");
+
+        int posicaoJogador1 = 0, posicaoJogador2 = 0, numeroRodada = 0;
+
+        do
+        {
+            numeroRodada += 1;
+            posicaoJogador1 = RodadaDoJogador(numeroRodada, posicaoJogador1);
+
+            posicaoJogador2 = RodadaDoComputador(posicaoJogador2);
+
+            if (posicaoJogador1 >= 30)
+            {
+                Console.WriteLine("Parabéns Jogador 1! Você ganhou!");
+                break;
+            }
+            else if (posicaoJogador2 >= 30)
+            {
+                Console.WriteLine("Parabéns Jogador 2! Você ganhou!");
+                break;
+            }
+
+        } while (posicaoJogador1 < 30 && posicaoJogador2 < 30);
+
+        Console.ReadLine();
+    }
+
+
+    static int RodadaDoJogador(int numeroRodada, int posicaoJogador1)
+    {
+        Console.WriteLine("\nPressione ENTER para começar a próxima rodada!");
+        Console.ReadLine();
+
+        Console.WriteLine($"Rodada nº: {numeroRodada}");
+
+        Random dado = new Random();
+        int dadoJogador1 = dado.Next(1, 7);
         posicaoJogador1 += dadoJogador1;
+
+        if (dadoJogador1 == 6)
+        {
+            Console.WriteLine($"Legal, você tirou {dadoJogador1} e ganhou uma rodada extra.");
+            dadoJogador1 = dado.Next(1, 7);
+            posicaoJogador1 += dadoJogador1;
+        }
+
+        if (posicaoJogador1 == 5 || posicaoJogador1 == 10 || posicaoJogador1 == 15)
+        {
+            Console.WriteLine($"Legal Jogador 1! Você parou na posição: {posicaoJogador1} , avance 03 casas!");
+            posicaoJogador1 += 3;
+        }
+        else if (posicaoJogador1 == 7 || posicaoJogador1 == 13 || posicaoJogador1 == 20 || posicaoJogador1 == 29)
+        {
+            Console.WriteLine($"Pena Jogador 1! Você parou na posição: {posicaoJogador1} , recue 02 casas!");
+            posicaoJogador1 -= 2;
+        }
+
+        Console.WriteLine($"Posição do jogador 1: {posicaoJogador1}");
+        return posicaoJogador1;
     }
 
-    if (posicaoJogador1 == 5 || posicaoJogador1 == 10 || posicaoJogador1 == 15)
+    static int RodadaDoComputador(int posicaoJogador2)
     {
-        Console.WriteLine($"Legal Jogador 1! Você parou na posição: {posicaoJogador1} , avance 03 casas!");
-        posicaoJogador1 += 3;
-    }
-    else if (posicaoJogador1 == 7 || posicaoJogador1 == 13 || posicaoJogador1 == 20 || posicaoJogador1 == 29)
-    {
-        Console.WriteLine($"Pena Jogador 1! Você parou na posição: {posicaoJogador1} , recue 02 casas!");
-        posicaoJogador1 -= 2;
-    }
-
-    Console.WriteLine($"Posição do jogador 1: {posicaoJogador1}");
-
-    dadoJogador2 = dado.Next(1, 7);
-    posicaoJogador2 += dadoJogador2;
-
-    if (dadoJogador2 == 6)
-    {
-        Console.WriteLine($"Legal, você tirou {dadoJogador2} e ganhou uma rodada extra.");
-        dadoJogador2 = dado.Next(1, 7);
+        Random dado = new Random();
+        int dadoJogador2 = dado.Next(1, 7);
         posicaoJogador2 += dadoJogador2;
-    }
 
-    if (posicaoJogador2 == 5 || posicaoJogador2 == 10 || posicaoJogador2 == 15)
-    {
-        Console.WriteLine($"Legal Jogador 2! Você parou na posição: {posicaoJogador2} , avance 03 casas!");
-        posicaoJogador2 += 3;
-    }
-    else if (posicaoJogador2 == 7 || posicaoJogador2 == 13 || posicaoJogador2 == 20 || posicaoJogador2 == 29)
-    {
-        Console.WriteLine($"Pena Jogador 2! Você parou na posição: {posicaoJogador2} , recue 02 casas!");
-        if (posicaoJogador2 >= 2)
-            posicaoJogador2 -= 2;
-        else
-            posicaoJogador2 = 0;
-    }
+        if (dadoJogador2 == 6)
+        {
+            Console.WriteLine($"Legal, você tirou {dadoJogador2} e ganhou uma rodada extra.");
+            dadoJogador2 = dado.Next(1, 7);
+            posicaoJogador2 += dadoJogador2;
+        }
 
-    Console.WriteLine($"Posição do jogador 2: {posicaoJogador2}");
+        if (posicaoJogador2 == 5 || posicaoJogador2 == 10 || posicaoJogador2 == 15)
+        {
+            Console.WriteLine($"Legal Jogador 2! Você parou na posição: {posicaoJogador2} , avance 03 casas!");
+            posicaoJogador2 += 3;
+        }
+        else if (posicaoJogador2 == 7 || posicaoJogador2 == 13 || posicaoJogador2 == 20 || posicaoJogador2 == 29)
+        {
+            Console.WriteLine($"Pena Jogador 2! Você parou na posição: {posicaoJogador2} , recue 02 casas!");
+            if (posicaoJogador2 >= 2)
+                posicaoJogador2 -= 2;
+            else
+                posicaoJogador2 = 0;
+        }
 
-    if (posicaoJogador1 >= 30)
-    {
-        Console.WriteLine("Parabéns Jogador 1! Você ganhou!");
-        break;
+        Console.WriteLine($"Posição do jogador 2: {posicaoJogador2}");
+        return posicaoJogador2;
     }
-    else if (posicaoJogador2 >= 30)
-    {
-        Console.WriteLine("Parabéns Jogador 2! Você ganhou!");
-        break;
-    }
-
-} while (posicaoJogador1 < 30 && posicaoJogador2 < 30);
-
-Console.ReadLine();
+}
